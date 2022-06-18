011549d72e5b6a71d58262966e413fc01ded7465
sani2608 <sanikumarsahani@gmail.com>
2022-06-18 16:53:49 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Box, Stack } from "@mui/material";
+import Feed from "components/Feed";
+import Navbar from "components/Navbar";
+import Rightbar from "components/Rightbar";
+import Sidebar from "components/Sidebar";
+
 function App() {
-  return <div className='App'>this is awesome</div>;
+  return (
+    <Box>
+      <Navbar />
+      <Stack direction='row' spacing={2} justifyContent='space-between'>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
