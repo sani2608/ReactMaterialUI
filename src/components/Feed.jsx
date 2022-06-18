c0d95edf84c8f952e295cdc6e24098dfe5a37054
sani2608 <sanikumarsahani@gmail.com>
2022-06-18 18:10:14 +0530

    dark theme skeleton loading

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Skeleton, Stack } from "@mui/material";
 import Post from "components/Post";
 
 const Feed = () => {
   const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, [3000]);
+
     const fetchData = async () => {
       const data = await fetch("https://picsum.photos/v2/list?page=2&limit=20");
       const posts = await data.json();
@@ -17,8 +22,27 @@ const Feed = () => {
 
   return (
     <Box flex={4} p={2}>
-      {post &&
-        post.map((post) => <Post key={post.id} author={post.author} image={post.url} />)}
+      {loading ? (
+        <Stack spacing={1}>
+          <Skeleton variant='text' height={100} />
+          <Skeleton variant='text' height={20} />
+          <Skeleton variant='text' height={20} />
+          <Skeleton variant='rectangular' height={300} />
+          <Skeleton variant='text' height={20} />
+          <Skeleton variant='rectangular' height={300} />
+          <Skeleton variant='text' height={20} />
+          <Skeleton variant='rectangular' height={300} />
+          <Skeleton variant='text' height={20} />
+          <Skeleton variant='rectangular' height={300} />
+        </Stack>
+      ) : (
+        <>
+          {post &&
+            post.map((post) => (
+              <Post key={post.id} author={post.author} image={post.url} />
+            ))}
+        </>
+      )}
     </Box>
   );
 };
